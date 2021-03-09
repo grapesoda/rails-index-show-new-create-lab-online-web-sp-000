6fadb4f0ffbdbb0142a095f99896210ee595b10c
Tres Tarwater <trest03@gmail.com>
2021-03-09 10:41:29 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
-  resources :coupons, only: [:index, :new, :create]
-  get '/coupon/:id', to: 'coupons#show', as: 'coupon'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,6 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  
-  
+  resources :coupons, only: [:index, :show, :new, :create]
 end
